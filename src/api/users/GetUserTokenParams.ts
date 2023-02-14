fb7e73839fae8d14fe49e12d64edf47926e71bf8
Will Hohyon Ryu <will@oxopolitics.com>
2023-02-13 20:50:51 -0800

    Added GetUserTokenParams.ts

---
+++ src/api/users/GetUserTokenParams.ts
@@ -0,0 +1,8 @@
+// Get the diverse user token for the current user with the Bearer token.
+export interface GetUserTokenRequestParams {
+
+}
+
+export interface GetUserTokenResponseParams {
+  token: string;
+}
